d656eba1761330fe00e3c9513b3856c1d9f095e9
Sumit Saini <sumit.saini@nagarro.com>
2019-06-04 16:03:33 +0530

    changed values

--- playermanagement/Controllers/ValuesController.cs
+++ playermanagement/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace playermanagement.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "lets", "go" };
         }
 
         // GET api/values/5
