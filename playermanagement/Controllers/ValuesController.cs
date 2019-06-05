ca5b73ef98723f86d874fab42a13ad977514e687
Sumit Saini <sumit.saini@nagarro.com>
2019-06-05 09:40:02 +0530

    changed routes

--- playermanagement/Controllers/ValuesController.cs
+++ playermanagement/Controllers/ValuesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace playermanagement.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("players/api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
