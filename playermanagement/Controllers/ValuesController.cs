2aab1523b4eac5c1cb43b677c2d7694c4d557623
Sumit Saini <sumit.saini@nagarro.com>
2019-06-14 11:02:34 +0530

    asdd

--- playermanagement/Controllers/ValuesController.cs
+++ playermanagement/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace playermanagement.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "inside ", "player test" };
+            return new string[] { "inside test ", "player" };
         }
 
         // GET api/values/5
